c07fab7ca7b3a9a77806fcc35b1e55738497a1bd
Apon <flapon808@gmail.com>
2022-10-07 19:44:37 +0600

    The project

---
+++ TheRestaurantReviews/accounts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def signin(request):
+    return render(request, 'signin.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+# Create your views here.
